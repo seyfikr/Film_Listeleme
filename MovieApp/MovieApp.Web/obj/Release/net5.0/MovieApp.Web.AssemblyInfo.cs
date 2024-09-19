b30db0e48290c26755870d17ed6643d3f8bc0de9
seyfikr <seyfikr5580@gmail.com>
2024-09-19 11:14:00 +0300

    .

--- MovieApp/MovieApp.Web/obj/Release/net5.0/MovieApp.Web.AssemblyInfo.cs
+++ MovieApp/MovieApp.Web/obj/Release/net5.0/MovieApp.Web.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp.Web")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9040dba1bb4b38bdb664a4d700dbdacb04f8c65a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp.Web")]
